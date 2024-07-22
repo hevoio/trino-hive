4bf9aea83ddd93aeb4db04a04a120dfb60679e1e
Harshit Karira <harshit.karira@hevodata.com>
2024-07-22 20:12:11 +0530

    feat/DEST-357/configs-for-trino: added ci changes

--- .deployment/build-trino-image.sh
+++ .deployment/build-trino-image.sh
@@ -5,10 +5,7 @@ set -ex
 echo "CURRENT WORKING DIRECTORY :: ${PWD}"
 echo "S3_TRINO_HIVE_PROPERTIES_PATH  :: ${S3_TRINO_HIVE_PROPERTIES_PATH}"
 echo "S3_TRINO_NODE_PROPERTIES_PATH :: ${S3_TRINO_NODE_PROPERTIES_PATH}"
-#aws s3 cp ${S3_TRINO_HIVE_PROPERTIES_PATH} ${PWD}/etc/hive.properties
-echo "Downloaded S3_TRINO_HIVE_PROPERTIES_FILE"
-#aws s3 cp ${S3_TRINO_NODE_PROPERTIES_PATH} ${PWD}/etc/node.properties
-echo "Downloaded S3_TRINO_NODE_PROPERTIES_FILE"
+
 echo "Current DIRECTORY Contents :: `ls -lrth`"
 
 image_tag=$(echo "$CIRCLE_BRANCH" | sed -e "s/\//_/g")
