b4ba6c0e336cb7b0fe2893c513a7e98921dba0c8
Harshit Karira <harshit.karira@hevodata.com>
2024-07-22 15:10:06 +0530

    feat/DEST-357/configs-for-trino: CI changes trino

--- .deployment/start.sh
+++ .deployment/start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 aws s3 cp ${S3_TRINO_HIVE_PROPERTIES_PATH} /etc/trino/catalog/hive.properties
+cat /etc/trino/catalog/hive.properties
 aws s3 cp ${S3_TRINO_NODE_PROPERTIES_PATH} /etc/trino/node.properties
+cat /etc/trino/catalog/node.properties
 
 /usr/lib/trino/bin/run-trino
